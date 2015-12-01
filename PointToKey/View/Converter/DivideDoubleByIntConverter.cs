c51a4305d9e53f72d4ea0153e9c9ac07ef97937b
Nigel Jones <nigejones@gmail.com>
2015-12-01 08:52:39 +0000

    Rows and cols bound

---
+++ PointToKey/View/Converter/DivideDoubleByIntConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PointToKey.View.Converter
+{
+    class DivideDoubleByIntConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var value0 = System.Convert.ToDouble(values[0]);
+            if (double.IsNaN(value0))
+            {
+                return 0.0;
+            }
+            
+            var value1 = System.Convert.ToInt32(values[1]);
+
+            return value0 / value1;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
